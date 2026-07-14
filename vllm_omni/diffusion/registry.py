404f5c18b4bf77b1f9d82c875704048d84e11753
zhangbin.327 <zhangbin.327@bytedance.com>
2026-07-14 17:26:42 +0800

    refactor: align LingBot World v2 contracts
    Signed-off-by: zhangbin.327 <zhangbin.327@bytedance.com>

--- vllm_omni/diffusion/registry.py
+++ vllm_omni/diffusion/registry.py
@@ -348,6 +348,7 @@ _NO_CACHE_ACCELERATION = {
     # Pipelines that do not support cache acceleration (cache_dit / tea_cache).
     "NextStep11Pipeline",
     "AudioXPipeline",
+    "LingBotWorldCausalDMDPipeline",
 }
 
 
