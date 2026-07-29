9cf87b5b98476f4ddfb776a4f94f63062015ef8c
Jack47 <scsvip@gmail.com>
2026-07-29 23:52:40 +0800

    [Model] Organize LingBot World modules
    Signed-off-by: Jack47 <scsvip@gmail.com>

--- vllm_omni/diffusion/registry.py
+++ vllm_omni/diffusion/registry.py
@@ -112,7 +112,7 @@ _DIFFUSION_MODELS = {
         "WanI2VDMD2Pipeline",
     ),
     "LingBotWorldCausalDMDPipeline": (
-        "wan2_2.lingbot_world",
+        "lingbot_world",
         "pipeline",
         "LingBotWorldCausalDMDPipeline",
     ),
