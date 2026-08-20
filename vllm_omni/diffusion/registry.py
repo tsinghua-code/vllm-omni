37088f9da1e1f1fb18f3e9a0488240c5408a8044
tsinghua-code <zr-liu25@mails.tsinghua.edu.cn>
2026-08-20 09:46:52 +0800

    [Model] Add ABot-World realtime AR-Diffusion support
    Signed-off-by: tsinghua-code <zr-liu25@mails.tsinghua.edu.cn>

--- vllm_omni/diffusion/registry.py
+++ vllm_omni/diffusion/registry.py
@@ -169,6 +169,11 @@ _DIFFUSION_MODELS = {
         "pipeline",
         "LingBotWorldCausalDMDPipeline",
     ),
+    "ABotWorldCausalPipeline": (
+        "abot_world",
+        "pipeline",
+        "ABotWorldCausalPipeline",
+    ),
     "LongCatImagePipeline": (
         "longcat_image",
         "pipeline_longcat_image",
@@ -413,6 +418,7 @@ _NO_CACHE_ACCELERATION = {
     "Pi0Pipeline",
     "Pi05Pipeline",
     "LingBotWorldCausalDMDPipeline",
+    "ABotWorldCausalPipeline",
 }
 
 
@@ -611,6 +617,7 @@ _DIFFUSION_POST_PROCESS_FUNCS = {
     "WanT2VDMD2Pipeline": "get_wan22_post_process_func",
     "WanI2VDMD2Pipeline": "get_wan22_i2v_post_process_func",
     "LingBotWorldCausalDMDPipeline": "get_lingbot_world_post_process_func",
+    "ABotWorldCausalPipeline": "get_abot_world_post_process_func",
     "LongCatImagePipeline": "get_longcat_image_post_process_func",
     "LongCatVideoAvatarPipeline": "get_longcat_video_avatar_post_process_func",
     "BagelPipeline": "get_bagel_post_process_func",
@@ -679,6 +686,7 @@ _DIFFUSION_PRE_PROCESS_FUNCS = {
     "WanT2VDMD2Pipeline": "get_wan22_pre_process_func",
     "WanI2VDMD2Pipeline": "get_wan22_i2v_pre_process_func",
     "LingBotWorldCausalDMDPipeline": "get_lingbot_world_pre_process_func",
+    "ABotWorldCausalPipeline": "get_abot_world_pre_process_func",
     "OmniGen2Pipeline": "get_omnigen2_pre_process_func",
     "HeliosPipeline": "get_helios_pre_process_func",
     "HeliosPyramidPipeline": "get_helios_pre_process_func",
